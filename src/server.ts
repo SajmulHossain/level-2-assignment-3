5b8a9c963ad199e47c6511f84fe505f47689a78b
Sajmul <sajmul1427@gmail.com>
2025-06-21 17:25:53 +0600

    server is succesfully connected with mongodb with mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,25 @@
+import { Server } from "http";
+import dotenv from 'dotenv'
+import mongoose from "mongoose";
+import app from "./app";
 
-// let server: Server
+dotenv.config();
+
+let server: Server;
+const port: number = 3000;
+
+async function main() {
+    try {
+        await mongoose.connect(
+          `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.saftd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+        );
+
+        server = app.listen(port, () => {
+            console.log(`server is running on port: %s`, port);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main();
