0428ce18d8f59e757f44bf0eb1fe716b7d040ca4
Sajmul <sajmul1427@gmail.com>
2025-06-21 18:45:38 +0600

    all books getting api done

--- src/app/controllers/books.controller.ts
+++ src/app/controllers/books.controller.ts
@@ -1,25 +1,25 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response } from "express";
 import { Books } from "../models/book.model";
 
 export const bookRouter = express.Router();
 
-bookRouter.get("/api/books", async(req: Request, res: Response) => {
-    const books = await Books.find();
-    res.send({
-        success: true,
-        message: "Books retrieved successfully",
-        data: books
-    })
-})
+bookRouter.get("/", async (req: Request, res: Response) => {
+  const { filter, sortBy, sort, limit } = req.query;
+  const books = await Books.find({});
+  res.send({
+    success: true,
+    message: "Books retrieved successfully",
+    data: books,
+  });
+});
 
+bookRouter.post("", async (req: Request, res: Response) => {
+  const { body } = req;
+  const book = await Books.create(body);
 
-bookRouter.post("", async(req: Request, res: Response) => {
-    const { body} = req;
-    const book = await Books.create(body);
-
-    res.status(201).json({
-        success: true,
-        message: "Book created successfully",
-        data: book
-    })
-})
+  res.status(201).json({
+    success: true,
+    message: "Book created successfully",
+    data: book,
+  });
+});
